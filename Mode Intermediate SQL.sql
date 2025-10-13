e146825ee230d678c70e1041de156bacdf74e434
DoyeonKim <2001olleh@gmail.com>
2025-10-13 14:58:49 +0900

    Update Mode Intermediate SQL.sql

--- Mode Intermediate SQL.sql
+++ Mode Intermediate SQL.sql
@@ -1,9 +1,11 @@
 -- Mode SQL Intermediate
 
 -- COUNT(null이 아닌 행의 개수를 셈, 숫자가 아닌 열에도 사용 가능)
+-- 1번
 SELECT COUNT(low) AS low
 FROM tutorial.aapl_historical_stock_price
 
+-- 2번
 SELECT COUNT(year) AS year,
        COUNT(month) AS month,
        COUNT(open) AS open,
@@ -18,9 +20,11 @@ SELECT SUM(open)/COUNT(open) AS avg_opening_price
 FROM tutorial.aapl_historical_stock_price
 
 -- MIN/MAX(숫자가 아닌 열에도 사용 가능)
+-- 1번
 SELECT MIN(low)
 FROM tutorial.aapl_historical_stock_price
 
+-- 2번
 SELECT MAX(close - open)
 FROM tutorial.aapl_historical_stock_price
 
@@ -29,16 +33,19 @@ SELECT AVG(volume) as avg_volume
 FROM tutorial.aapl_historical_stock_price
 
 --GROUP BY
+-- 1번
 SELECT year, month, SUM(volume) AS sum_volume
 FROM tutorial.aapl_historical_stock_price
 GROUP BY year, month
 ORDER BY year, month
 
+-- 2번
 SELECT year, AVG(close-open) AS avg_daily_change
 FROM tutorial.aapl_historical_stock_price
 GROUP BY year
 ORDER BY year
 
+--3번
 SELECT year, month, MAX(high) AS highest_price, MIN(low) AS lowest_price
 FROM tutorial.aapl_historical_stock_price
 GROUP BY year, month
@@ -52,6 +59,7 @@ ORDER BY year, month
 -- SQL에서 사용하는 if/then 로직
 -- WHEN ~ THEN 절이 적어도 한쌍 이상은 따라와야함, ELSE는 선택적
 -- END 절로 끝냄
+-- 1번
 SELECT player_name,
        state,
        CASE WHEN state = 'CA' THEN 'yes'
@@ -59,7 +67,7 @@ SELECT player_name,
 FROM benn.college_football_players
 ORDER BY 3
 
--- 집계함수와 함께 CASE 사용       
+-- 2번 (집계함수와 함께 CASE 사용)  
 SELECT *
        CASE WHEN year IN('JR', 'SR') THEN player_name ELSE NULL END AS upperclass_player_name
 FROM benn.college_football_players
@@ -72,6 +80,7 @@ SELECT CASE WHEN state IN ('CA', 'OR', 'WA') THEN 'West Coast'
  WHERE weight >= 300
  GROUP BY 1
 
+-- 3번
 SELECT CASE WHEN year IN ('FR', 'SO') THEN 'underclass'
             WHEN year IN ('JR', 'SR') THEN 'upperclass'
             ELSE NULL END AS class_group,
@@ -80,7 +89,7 @@ SELECT CASE WHEN year IN ('FR', 'SO') THEN 'underclass'
  WHERE state = 'CA'
  GROUP BY 1
 
--- 집계함수 내부에서 CASE 사용       
+-- 4번 (집계함수 내부에서 CASE 사용)       
 SELECT state,
        COUNT(CASE WHEN year = 'FR' THEN 1 ELSE NULL END) AS fr_count,
        COUNT(CASE WHEN year = 'SO' THEN 1 ELSE NULL END) AS so_count,
@@ -91,6 +100,7 @@ SELECT state,
  GROUP BY state
  ORDER BY total_players DESC
 
+-- 5번
 SELECT CASE WHEN school_name < 'n' THEN 'A-M'
             WHEN school_name >= 'n' THEN 'N-Z'
             ELSE NULL END AS school_name_group,
@@ -100,17 +110,19 @@ SELECT CASE WHEN school_name < 'n' THEN 'A-M'
 
 -- DISTINCT
 -- 고유한 값을 보기 위해서 사용, 한번만 포함하면 됨
+-- 1번
 SELECT DISTINCT year
 FROM tutorial.aapl_historical_stock_price
 ORDER BY year
 
--- 집계에서 DISTINCT 사용
+-- 2번 (집계에서 DISTINCT 사용)
 -- MAX, MIN과는 사용하면 안됨
 SELECT COUNT(DISTINCT month) AS unique_values
 FROM tutorial.aapl_historical_stock_price
 GROUP BY year
 ORDER BY year
 
+-- 3번
 SELECT COUNT(DISTINCT month) AS month_values,
        COUNT(DISTINCT year) AS year_values
 FROM tutorial.aapl_historical_stock_price
@@ -141,18 +153,21 @@ SELECT players.player_name,
 -- FULL OUTER JOIN은 두 테이블 모두에서 일치하지 않는 행 과 두 테이블 모두에서 일치하는 행을 반환
 
 -- LEFT JOIN
+-- 1번
 SELECT COUNT(companies.permalink) AS companies_rowcount,
        COUNT(acquisitions.company_permalink) AS acquisitions_rowcount
   FROM tutorial.crunchbase_companies companies
   JOIN tutorial.crunchbase_acquisitions acquisitions
     ON companies.permalink = acquisitions.company_permalink
 
+-- 2번
 SELECT COUNT(companies.permalink) AS companies_rowcount,
        COUNT(acquisitions.company_permalink) AS acquisitions_rowcount
   FROM tutorial.crunchbase_companies companies
   LEFT JOIN tutorial.crunchbase_acquisitions acquisitions
     ON companies.permalink = acquisitions.company_permalink
 
+-- 3번
 SELECT companies.state_code,
        COUNT(DISTINCT companies.permalink) AS unique_companies,
        COUNT(DISTINCT acquisitions.company_permalink) AS unique_companies_acquired
@@ -178,6 +193,7 @@ SELECT companies.state_code,
 
 -- Joins using WHERE or ON
 -- WHERE 절 사용시 테이블이 조인된 후에 필터가 적용됨
+-- 1번
 SELECT companies.name AS company_name,
        companies.status,
        COUNT(DISTINCT investments.investor_name) AS unqiue_investors
@@ -188,6 +204,7 @@ SELECT companies.name AS company_name,
  GROUP BY 1,2
  ORDER BY 3 DESC
 
+-- 2번
 SELECT CASE WHEN investments.investor_name IS NULL THEN 'No Investors'
             ELSE investments.investor_name END AS investor,
        COUNT(DISTINCT companies.permalink) AS companies_invested_in
@@ -212,6 +229,7 @@ SELECT CASE WHEN investments.investor_name IS NULL THEN 'No Investors'
 -- UNION
 -- 고유한 값만 추가
 -- 추가된 테이블에서 첫 번째 테이블의 행과 정확히 동일한 행은 삭제
+-- 1번
 SELECT company_permalink,
        company_name,
        investor_name
@@ -226,6 +244,7 @@ SELECT company_permalink,
   FROM tutorial.crunchbase_investments_part2
  WHERE company_name ILIKE 'M%'
 
+-- 2번
 SELECT 'investments_part1' AS dataset_name,
        companies.status,
        COUNT(DISTINCT investments.investor_permalink) AS investors
